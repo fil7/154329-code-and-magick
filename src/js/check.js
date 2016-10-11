2d6e3f6aa4605e2552fb321b267bb7fd3cabbe6d
fil7 <filonova7@yandex.ru>
2016-10-11 19:36:01 +0700

    Reverted if-else

--- src/js/check.js
+++ src/js/check.js
@@ -1,11 +1,21 @@
-"use strict";
+'use strict';
+
 function getMessage(a, b) {
-	return (a === true) ? 'Я попал в ' + b : 
-				(a === false) ? 'Я никуда не попал' :
-					(typeof a === "number") ? 'Я прыгнул на ' + (a * 100) + ' сантиметров' :
-						(Array.isArray(b) && Array.isArray(a)) ? 'Я прошёл ' + getDistancePath(a, b) + ' метров' :
-							(Array.isArray(a)) ? 'Я прошёл ' + getNumberOfSteps(a) + ' шагов' :							
-								'Переданы некорректные данные';
+  var resultMessage = '';
+  if (a === true) {
+    resultMessage = 'Я попал в ' + b;
+  } else if (a === false) {
+    resultMessage = 'Я никуда не попал';
+  } else if (typeof a === "number") {
+    resultMessage = 'Я прыгнул на ' + (a * 100) + ' сантиметров';
+  } else if (Array.isArray(b) && Array.isArray(a)) {
+    resultMessage = 'Я прошёл ' + getDistancePath(a, b) + ' метров';
+  } else if (Array.isArray(a)) {
+    resultMessage = 'Я прошёл ' + getNumberOfSteps(a) + ' шагов';
+  } else {
+    resultMessage = 'Переданы некорректные данные';
+  }
+  return resultMessage;
 }
 
 function getNumberOfSteps(a) {
