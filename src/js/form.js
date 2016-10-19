f4eef5457e40d9c9ecca01391fd16b2cab86332d
fil7 <filonova7@yandex.ru>
2016-10-19 12:00:42 +0700

    12 Валидация: рефакторинг form.js

--- src/js/form.js
+++ src/js/form.js
@@ -5,12 +5,10 @@ window.form = (function() {
   var formCloseButton = document.querySelector('.review-form-close');
   var formSubmitButton = document.querySelector('.review-submit');
 
-  var fieldName = document.getElementById('review-name');
-  var fieldText = document.getElementById('review-text');
-  var fieldsetMark = document.querySelector('.review-form-group-mark');
-  var labelsMandatory = document.querySelector('.review-fields');
-  var labelMandatoryText = document.querySelector('.review-fields-text');
-
+  var commentField = document.getElementById('review-text');
+  var markFieldset = document.querySelector('.review-form-group-mark');
+  var mandatoryLabels = document.querySelector('.review-fields');
+  var commentLabelMandatory = document.querySelector('.review-fields-text');
   var textFields = document.querySelectorAll('.review-form-field');
 
   var form = {
@@ -21,15 +19,15 @@ window.form = (function() {
      */
     open: function(cb) {
       formContainer.classList.remove('invisible');
-      setMandatoryFields();
+      document.getElementById('review-name').required = true;
       setFieldListeners();
       formSubmitButton.disabled = true;
+      commentLabelMandatory.hidden = true;
       cb();
     },
 
     close: function() {
       formContainer.classList.add('invisible');
-
       if (typeof this.onClose === 'function') {
         this.onClose();
       }
@@ -41,54 +39,40 @@ window.form = (function() {
     form.close();
   };
 
-  function setMandatoryFields() {
-    fieldName.required = true;
-    var mark = document.querySelector('input[type="radio"]:checked + .review-mark-label').innerHTML;
-    setTextMandatoryIf(mark);
-  }
-
-  function setTextMandatoryIf(mark) {
-    fieldText.required = mark < 3;
-    labelMandatoryText.hidden = fieldText.validity.valid;
-  }
-
   function setFieldListeners() {
-
     for (var i = 0; i < textFields.length; i++) {
-      textFields[i].addEventListener('input', textFieldHandler);
+      textFields[i].addEventListener('input', checkForm);
     }
-
-    fieldsetMark.onchange = function(e) {
-      if (e.target.tagName !== 'INPUT') {
-        return;
-      }
-      if (e.target.labels[0]) {
-        var mark = e.target.labels[0].innerHTML;
-        setTextMandatoryIf(mark);
-        checkMandatoryFields();
-      }
-    };
-
-    function textFieldHandler(e) {
-      var field = e.target;
-      var labels = e.target.labels || [];
-      for (var j = 0; j < labels.length; j++) {
-        if (labels[j].classList.contains('review-fields-label')) {
-          labels[j].hidden = field.validity.valid;
-          break;
-        }
-      }
-      checkMandatoryFields();
-    }
+    markFieldset.addEventListener('change', checkForm);
   }
 
-  function checkMandatoryFields() {
-    var isFormValid = true;
-    for (var i = 0; i < textFields.length; i++) {
-      isFormValid &= textFields[i].validity.valid;
+  function checkForm(e) {
+    var evt = e || event;
+    var field = evt.target;
+    var labels = evt.target.labels || [];
+
+    for (var j = 0; j < labels.length; j++) {
+      var label = labels[j];
+      // check comment mandatory
+      if(label.classList.contains('review-mark-label')) {
+        var mark = label.innerHTML;
+        commentField.required = mark < 3;
+        commentLabelMandatory.hidden = commentField.validity.valid;
+        // hide label-mandatory for text input
+      } else if (label.classList.contains('review-fields-label')) {
+        label.hidden = field.validity.valid;
+      }
     }
-    labelsMandatory.style.display = isFormValid ? 'none' : 'inline-block';
-    formSubmitButton.disabled = !isFormValid;
+    checkSubmitButton();
+
+    function checkSubmitButton() {
+      var isFormValid = true;
+      for (var i = 0; i < textFields.length; i++) {
+        isFormValid &= textFields[i].validity.valid;
+      }
+      mandatoryLabels.style.display = isFormValid ? 'none' : 'inline-block';
+      formSubmitButton.disabled = !isFormValid;
+    }
   }
 
   return form;
